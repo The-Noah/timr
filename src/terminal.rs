06198413888786d967e1dc0fef4ce3eb3d8b1244
The Noah <thenoahbz@gmail.com>
2024-09-27 15:11:34 -0500

    chore: improve docs

--- src/terminal.rs
+++ src/terminal.rs
@@ -3,22 +3,26 @@ pub fn previous_line() {
   print!("{}[F", 27 as char);
 }
 
+/// Clear the current line of all characters
 pub fn clear_line() {
-  // Move the cursor to the beginning of the line
+  // move the cursor to the beginning of the line
   print!("\r");
 
-  // Print whitespace characters to clear the line
+  // print whitespace characters to clear the line
   for _ in 0..get_width() {
     print!(" ");
   }
 
+  // reset back to beginning of line
   print!("\r");
 }
 
+/// Get the ANSI code to color the foreground in `red`, `green`, `blue`
 pub fn ansi_rgb(red: u8, green: u8, blue: u8) -> String {
   format!("{}[38;2;{red};{green};{blue}m", 27 as char)
 }
 
+/// Get the terminal's column count
 pub fn get_width() -> u16 {
   termsize::get().unwrap_or(termsize::Size { rows: 10, cols: 80 }).cols
 }
